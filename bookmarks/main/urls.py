d2de81dd3db688d5c401ffcfea15c91da6991d9c
Jessica Yuen <jyuen@ualberta.ca>
2015-03-04 18:06:11 -0700

    Adding part 2

--- bookmarks/main/urls.py
+++ bookmarks/main/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns(
     "",
     url(r'^$', views.index, name='index'),
     url(r'^tags/$', views.tags, name='tags'),
-    url(r'^tags/(?P<tag_name>\w+)/$', views.tags, name='tag'),
+    url(r'^tags/(?P<tag_name>\w+)/$', views.tag, name='tag'),
     url(r'^add_link/$', views.add_link, name='add_link'),
 )
